e122740f2886a6ea98be07db681bf777ff3f6d90
pymmog <pymmog@icloud.com>
2025-07-12 00:56:24 +0200

    add nvim

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "auto", -- auto, main, moon, or dawn
+      })
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+}
